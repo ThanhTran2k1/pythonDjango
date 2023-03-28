75f3df14637b7784d2e4fc45e722b5a26226a390
ThanhTran2k1 <tranquocthanhd19ptiter@gmail.com>
2023-03-28 15:32:50 +0700

    deploy

--- member/UserMember/forms.py
+++ member/UserMember/forms.py
@@ -26,11 +26,12 @@ class registerForm(forms.Form):
 
 class loginForm(forms.Form):
     username = forms.CharField(max_length=20,widget=forms.TextInput(attrs={
-        "class" : "form-input",
+        "class" : "form-input tendn",
         "placeholder" : "Tên đăng nhập"
+
     }))
     password = forms.CharField(max_length=20, widget=forms.PasswordInput(attrs={
-        "class" : "form-input",
+        "class" : "form-input recss",
         "placeholder" : "Mật khẩu"
     }))
 
